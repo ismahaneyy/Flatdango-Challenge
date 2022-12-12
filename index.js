fe1f6f7191b7a4a3c21765f01d0baf00ad103f32
ismahaneyy <liciousmayrah@gmail.com>
2022-12-12 09:32:51 +0300

    updated index.js

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ const baseUrl = "http://localhost:3000/films";
 function fetchMovies(){
     fetch(baseUrl)
         .then((response) => response.json()) 
-        .then((data) =)}
+        .then((data) =>{
+            data.forEach((films) =>{
+// Adds the film titles to the DOM in the form of a clickable menu
+              let li = document.createElement("li");
+              li.textContent = films.title;
+              li.addEventListener("click",
+              (e)=>{let buttonContent =
+                document.querySelector("button#buy-ticket")
+                buttonContent.textContent = "Buy Tickets"
 
 
