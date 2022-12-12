f80b667e1e1990b1e6b8ec8075f5983138d3ae73
ismahaneyy <liciousmayrah@gmail.com>
2022-12-12 09:41:11 +0300

    updated index.js

--- index.js
+++ index.js
@@ -41,18 +41,29 @@ function fetchMovies(){
          })
      }fetchMovies()
 
-    //Adds the first movie to the DOM to be added when the page loads
-     function baseMovie(){ fetch(baseUrl)
-       .then(response => response.json())
-       .then(data => {
-       document.querySelector("h3#movie-title").textContent = data[0]["title"]
-       document.querySelector("img#movie-poster").setAttribute("src",`${data[0]["poster"]}`)
-       document.querySelector("div#showtime").textContent = data[0]["showtime"]
-       document.querySelector("div#runtime").textContent = `${data[0]["runtime"]} Minutes`
-       document.querySelector("ul#films").firstElementChild.remove()
-       document.querySelector("div#ticket-counter").textContent = data[0]["capacity"] - data[0]["tickets_sold"]
-       })
-       }
-       baseMovie()
-
+//Adds the first movie to the DOM to be added when the page loads
+function baseMovie(){ fetch(baseUrl)
+    .then(response => response.json())
+    .then(data => {
+    document.querySelector("h3#movie-title").textContent = data[0]["title"]
+    document.querySelector("img#movie-poster").setAttribute("src",`${data[0]["poster"]}`)
+    document.querySelector("div#showtime").textContent = data[0]["showtime"]
+    document.querySelector("div#runtime").textContent = `${data[0]["runtime"]} Minutes`
+    document.querySelector("ul#films").firstElementChild.remove()
+    document.querySelector("div#ticket-counter").textContent = data[0]["capacity"] - data[0]["tickets_sold"]
+     })
+     }
+    baseMovie()
+//Reduces the amount of tickets in the tickets counter when the buy movies button is clicked
+function buyTicket(){
+    let button = document.querySelector("button#buy-ticket")
+    button.addEventListener("click",function(){
+    let currentLi = document.querySelector("div#ticket-counter")
+    let number = parseInt(currentLi.textContent)
+    if(number > 0){
+    currentLi.textContent = currentLi.textContent -1}
+    else{document.querySelector("button#buy-ticket").textContent = "TICKETS SOLD OUT"
+  }
+    })}
+  buyTicket()
 
