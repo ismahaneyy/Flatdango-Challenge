ec1d2ea35ca82624dbd46231a4107fd5d45164e0
ismahaneyy <liciousmayrah@gmail.com>
2022-12-12 09:39:36 +0300

    updated index.js

--- index.js
+++ index.js
@@ -41,4 +41,18 @@ function fetchMovies(){
          })
      }fetchMovies()
 
+    //Adds the first movie to the DOM to be added when the page loads
+     function baseMovie(){ fetch(baseUrl)
+       .then(response => response.json())
+       .then(data => {
+       document.querySelector("h3#movie-title").textContent = data[0]["title"]
+       document.querySelector("img#movie-poster").setAttribute("src",`${data[0]["poster"]}`)
+       document.querySelector("div#showtime").textContent = data[0]["showtime"]
+       document.querySelector("div#runtime").textContent = `${data[0]["runtime"]} Minutes`
+       document.querySelector("ul#films").firstElementChild.remove()
+       document.querySelector("div#ticket-counter").textContent = data[0]["capacity"] - data[0]["tickets_sold"]
+       })
+       }
+       baseMovie()
+
 
