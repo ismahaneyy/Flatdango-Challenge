2bb58aec21aad2b549bcd6e8ac3c1d3001c93760
ismahaneyy <liciousmayrah@gmail.com>
2022-12-12 09:31:40 +0300

    updated index.js

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-
+// fetches  a list of movies from the db.json file
+const baseUrl = "http://localhost:3000/films";
+function fetchMovies(){
+    fetch(baseUrl)
+        .then((response) => response.json()) 
+        .then((data) =)}
 
 
